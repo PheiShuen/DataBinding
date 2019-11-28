666385d2cecd8b34933d306407ac0621b499e6f1
Phei Shuen <chuahps-wm17@student.tarc.edu.my>
2019-11-28 11:18:50 +0800

    initial commit

---
+++ app/src/main/java/com/example/databinding/Contact.kt
@@ -0,0 +1,5 @@
+package com.example.databinding
+
+data class contact (var name: String ="", var phone: String = ""){
+
+}
--- app/src/main/java/com/example/databinding/MainActivity.kt
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -4,14 +4,30 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.view.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val myContact = contact("See", "0123456789")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //Display UI
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        //Assion attribute of local variable to UI variable
+        binding.contact = myContact
+        binding.buttonUpdate.setOnClickListener{
+            binding.apply{
+                contact?.name = "My new name"
+                contact?.phone = "1111111"
+                invalidateAll()
+            }
+        }
         //Create an event handler for buttonSend
         buttonSend.setOnClickListener {
             sendMesaage()
